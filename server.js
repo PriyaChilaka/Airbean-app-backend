20b27714430848d08019bbb6d9bcf194eb0e8aec
PriyaChilaka <priyakolukuluri@gmail.com>
2021-06-08 16:43:26 +0200

    commits

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const lowdb = require('lowdb')
+const cors = require('cors')
 const express = require('express')
 
 const coffeeRouter = require('./routes/index')
 const { initiateDatabase } = require('./handlers/database')
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use('/api/', coffeeRouter)
 
