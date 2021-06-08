8e9a5fb19239dedd894dd604c0eaf68f2abc0425
PriyaChilaka <priyakolukuluri@gmail.com>
2021-06-08 15:51:59 +0200

    server started

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-const lowdb = require('lowdb');
-
-const express = require('express');
-
-const FileSync = require('lowdb/adapters/FileSync')
+const lowdb = require('lowdb')
+const express = require('express')
 
 const coffeeRouter = require('./routes/index')
 const { initiateDatabase } = require('./handlers/database')
 
-const adapter = new FileSync('accounts.json')
-
-const database = lowdb(adapter)
-
 const app = express()
 
-//function initiateDatabase() {
- // database.defaults({ accounts: [] }).write()
-//
-app.use(express.json());
-app.use('/api/', coffeeRouter);
+app.use(express.json())
+app.use('/api/', coffeeRouter)
 
 app.listen(8000, () => {
-  console.log('Server started');
-  initiateDatabase();
+  console.log('Server started')
+  initiateDatabase()
 });
